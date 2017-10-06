b34eb70439a42361b542dffe7288158f1364bd9a
Auriwield <xronx11@gmail.com>
2017-10-07 01:11:31 +0300

    remove babel

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,22 @@
-"use strict";
-
 function onSelectOrDragImage() {
     return new window.Promise(function (resolve) {
-        $(".drop-zone:first").on("dragover", function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-            $(this).addClass('dragging');
-        }).on("dragleave", function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-            $(this).removeClass('dragging');
-        }).on("drop", function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-            var file = event.originalEvent.dataTransfer.files[0];
-            resolve(file);
-        });
+        $(".drop-zone:first")
+            .on("dragover", function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                $(this).addClass('dragging');
+            })
+            .on("dragleave", function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                $(this).removeClass('dragging');
+            })
+            .on("drop", function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+                var file = event.originalEvent.dataTransfer.files[0];
+                resolve(file);
+            });
 
         var input = $("input#fake-button");
 
@@ -31,20 +32,22 @@ function onSelectOrDragImage() {
 }
 
 $(document).ready(function () {
-    onSelectOrDragImage().then(function (file) {
-        var dropZone = $(".drop-zone:first");
-        var tabs = $("#tabs");
-        dropZone.addClass("hidden");
-        tabs.removeClass("hidden");
-        return canvasUtils.getImageData(file);
-    }).then(function (leftImageData) {
-        $("nav li a.active").first().parent().click();
-    });
+    onSelectOrDragImage()
+        .then(function (file) {
+            var dropZone = $(".drop-zone:first");
+            var tabs = $("#tabs");
+            dropZone.addClass("hidden");
+            tabs.removeClass("hidden");
+            return canvasUtils.getImageData(file);
+        })
+        .then(function (leftImageData) {
+            $("nav li a.active").first().parent().click();
+        });
 });
 
-var imageData = function imageData() {
+const imageData = (function () {
     return {
         left: null,
         right: null
-    };
-};
+    }
+});
--- public/js/utils/imageDataUtils.js
+++ public/js/utils/imageDataUtils.js
@@ -1,6 +1,4 @@
-"use strict";
-
-var canvasUtils = function () {
+const canvasUtils = (function () {
 
     // noinspection JSUnusedGlobalSymbols
     function getImage(canvas, imageData) {
@@ -9,72 +7,74 @@ var canvasUtils = function () {
     }
 
     function drawIntoCanvas(imageData, canvas, scale) {
-        var ctx = canvas[0].getContext("2d");
+        let ctx = canvas[0].getContext("2d");
         if (!scale) scale = calcScale(imageData);
         canvas[0].width = imageData.width * scale;
         canvas[0].height = imageData.height * scale;
 
-        if (scale !== 1) imageData = scaleImageData(imageData, scale);else imageData = new ImageData(imageData.data, imageData.width, imageData.height);
+        if (scale !== 1)
+            imageData = scaleImageData(imageData, scale);
+        else
+            imageData = new ImageData(imageData.data, imageData.width, imageData.height);
 
         ctx.putImageData(imageData, 0, 0);
     }
 
-    function makeCanvasSquare(canvas) {
-        var subw = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
-        var subh = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 0;
+    function makeCanvasSquare(canvas, subw = 0, subh = 0) {
+        let maxWidth = $(window).width() * 0.9 - subw;
+        let maxHeight = ($(window).height() - $(".header").height()) * 0.9 - subh;
 
-        var maxWidth = $(window).width() * 0.9 - subw;
-        var maxHeight = ($(window).height() - $(".header").height()) * 0.9 - subh;
-
-        var side = Math.min(maxWidth, maxHeight);
+        let side = Math.min(maxWidth, maxHeight);
 
         canvas[0].width = side;
         canvas[0].height = side;
     }
 
-    function calcScale(imageData) {
-        var multiplier = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 2;
-
-        var maxWidth = $(window).width() / multiplier * 0.9;
-        var maxHeight = ($(window).height() - $(".header").height()) * 0.9;
-        var mpX = maxWidth / imageData.width;
-        var mpY = maxHeight / imageData.height;
+    function calcScale(imageData, multiplier = 2) {
+        let maxWidth = $(window).width() / multiplier * 0.9;
+        let maxHeight = ($(window).height() - $(".header").height()) * 0.9;
+        let mpX = maxWidth / imageData.width;
+        let mpY = maxHeight / imageData.height;
         return Math.min(mpX, mpY);
     }
 
     function getPrescaledImageData() {
-        var maxWidth = Math.floor($(window).width() * 0.8);
-        var maxHeight = Math.floor(($(window).height() - $(".header").height()) * 0.8);
+        let maxWidth = Math.floor($(window).width() * 0.8);
+        let maxHeight = Math.floor(($(window).height() - $(".header").height()) * 0.8);
         return new ImageData(new Uint8ClampedArray(maxWidth * maxHeight * 4), maxWidth, maxHeight);
     }
 
     function scaleImageData(imageData, scale) {
-        var h1 = imageData.height;
-        var w1 = imageData.width;
-        var w2 = Math.floor(w1 * scale);
-        var h2 = Math.floor(h1 * scale);
+        let h1 = imageData.height;
+        let w1 = imageData.width;
+        let w2 = Math.floor(w1 * scale);
+        let h2 = Math.floor(h1 * scale);
 
-        var srcLength = h1 * w1 * 4;
-        var destLength = w2 * h2 * 4;
+        let srcLength = h1 * w1 * 4;
+        let destLength = w2 * h2 * 4;
 
-        var src = imageData.data;
-        var dest = new Uint8ClampedArray(destLength);
+        let src = imageData.data;
+        let dest = new Uint8ClampedArray(destLength);
 
-        for (var y = 0; y < h2; y++) {
-            for (var x = 0; x < w2; x++) {
-                var x1 = Math.floor(x / scale);
-                var y1 = Math.floor(y / scale);
+        for (let y = 0; y < h2; y++) {
+            for (let x = 0; x < w2; x++) {
+                let x1 = Math.floor(x / scale);
+                let y1 = Math.floor(y / scale);
 
-                if (x1 < 0 || x1 >= w1 || y1 < 0 || y1 >= h1) continue;
+                if (x1 < 0 || x1 >= w1 || y1 < 0 || y1 >= h1)
+                    continue;
 
-                if (x < 0 || x >= w2 || y < 0 || y >= h2) continue;
+                if (x < 0 || x >= w2 || y < 0 || y >= h2)
+                    continue;
 
-                var destIndex = (y * w2 + x) * 4;
-                var sourceIndex = (y1 * w1 + x1) * 4;
+                let destIndex = (y * w2 + x) * 4;
+                let sourceIndex = (y1 * w1 + x1) * 4;
 
-                if (destIndex + 3 >= destLength || destIndex < 0 || sourceIndex + 3 >= srcLength || sourceIndex < 0) continue;
+                if (destIndex + 3 >= destLength || destIndex < 0
+                    || sourceIndex + 3 >= srcLength || sourceIndex < 0) continue;
 
-                for (var i = 0; i < 4; i++) {
+
+                for (let i = 0; i < 4; i++) {
                     dest[destIndex + i] = src[sourceIndex + i];
                 }
             }
@@ -84,23 +84,23 @@ var canvasUtils = function () {
     }
 
     function applyBorder(imageData, t, color) {
-        var w = imageData.width + t;
-        var h = imageData.height + t;
+        let w = imageData.width + t;
+        let h = imageData.height + t;
 
-        var dest = new Uint8ClampedArray(w * h * 4);
+        let dest = new Uint8ClampedArray(w * h * 4);
 
-        for (var i = 0; i < imageData.height; i++) {
-            for (var j = 0; j < imageData.width; j++) {
-                var srcIndex = (i * imageData.width + j) * 4;
-                var destIndex = ((i + t) * w + j + t) * 4;
-                for (var p = 0; p < 4; p++) {
-                    dest[destIndex + p] = imageData.data[srcIndex + p];
+        for (let i = 0; i < imageData.height; i++) {
+            for (let j = 0; j < imageData.width; j++) {
+                let srcIndex = (i * imageData.width + j) * 4;
+                let destIndex = ((i + t) * w + j + t) * 4;
+                for (let p = 0; p < 4; p++) {
+                    dest[destIndex + p] = imageData.data[srcIndex + p]
                 }
                 destIndex += 4;
             }
         }
 
-        var data = new ImageData(dest, w, h);
+        let data = new ImageData(dest, w, h);
 
         fillRectangle(data, 0, 0, w, t, color);
         fillRectangle(data, 0, t, t, h - t * 2, color);
@@ -111,15 +111,15 @@ var canvasUtils = function () {
     }
 
     function fillRectangle(imageData, x, y, width, height, color) {
-        var yEnd = y + height;
-        var xEnd = x + width;
+        let yEnd = y + height;
+        let xEnd = x + width;
 
         if (yEnd > imageData.height) yEnd = imageData.height;
         if (xEnd > imageData.width) xEnd = imageData.width;
 
-        for (var i = y; i < yEnd; i++) {
-            for (var j = x; j < xEnd; j++) {
-                var index = (i * imageData.width + j) * 4;
+        for (let i = y; i < yEnd; i++) {
+            for (let j = x; j < xEnd; j++) {
+                let index = (i * imageData.width + j) * 4;
                 imageData.data[index] = color.r;
                 imageData.data[index + 1] = color.g;
                 imageData.data[index + 2] = color.b;
@@ -129,11 +129,11 @@ var canvasUtils = function () {
     }
 
     function getImageData(file) {
-        var url = window.URL.createObjectURL(file);
-        var canvas = document.createElement('canvas');
-        var ctx = canvas.getContext("2d");
-        var image = new Image();
-        var promise = new window.Promise(function (resolve) {
+        let url = window.URL.createObjectURL(file);
+        let canvas = document.createElement('canvas');
+        let ctx = canvas.getContext("2d");
+        let image = new Image();
+        let promise = new window.Promise(function (resolve) {
             image.onload = function () {
                 canvas.width = image.width;
                 canvas.height = image.height;
@@ -146,13 +146,14 @@ var canvasUtils = function () {
     }
 
     function onMouseDownDelta(canvas, callback) {
-        var x = -1;
-        var y = -1;
-        canvas.mousemove(function (e) {
+        let x = -1;
+        let y = -1;
+        canvas.mousemove((e) => {
 
-            if (e.which === 1 && x !== -1 && y !== -1) {
-                var dx = e.pageX - x;
-                var dy = e.pageY - y;
+            if (e.which === 1
+                && x !== -1 && y !== -1) {
+                let dx = e.pageX - x;
+                let dy = e.pageY - y;
                 callback(dx, dy);
             }
 
@@ -162,11 +163,11 @@ var canvasUtils = function () {
     }
 
     function onMouseDownAbsolute(canvas, callback) {
-        canvas.mousemove(function (e) {
-            var rect = canvas[0].getBoundingClientRect();
+        canvas.mousemove((e) => {
+            let rect = canvas[0].getBoundingClientRect();
             if (e.which === 1) {
-                var x = e.pageX - rect.left;
-                var y = e.pageY - rect.top;
+                let x = e.pageX - rect.left;
+                let y = e.pageY - rect.top;
                 callback(x, y);
             }
         });
@@ -179,50 +180,50 @@ var canvasUtils = function () {
     }
 
     function getSquareAtCoords(imageData, x, y) {
-        var w = 4;
-        var h = 4;
-        var dest = new Uint8ClampedArray(w * h * 4);
+        let w = 4;
+        let h = 4;
+        let dest = new Uint8ClampedArray(w * h * 4);
 
-        for (var i = 3; i < dest.length; i += 4) {
+        for (let i = 3; i < dest.length; i += 4) {
             dest[i] = 255;
         }
 
         x = x - x % 4;
         y = y - y % 4;
 
-        var xEnd = x + 4;
-        var yEnd = y + 4;
+        let xEnd = x + 4;
+        let yEnd = y + 4;
 
         if (xEnd > imageData.width) xEnd = imageData.width;
         if (yEnd > imageData.height) yEnd = imageData.height;
 
-        for (var _i = y, i1 = 0; _i < yEnd; _i++, i1++) {
-            for (var j = x, j1 = 0; j < xEnd; j++, j1++) {
-                var srcIndex = (_i * imageData.width + j) * 4;
-                var destIndex = (i1 * w + j1) * 4;
+        for (let i = y, i1 = 0; i < yEnd; i++, i1++) {
+            for (let j = x, j1 = 0; j < xEnd; j++, j1++) {
+                let srcIndex = (i * imageData.width + j) * 4;
+                let destIndex = (i1 * w + j1) * 4;
 
-                for (var p = 0; p < 3; p++) {
-                    dest[destIndex + p] = imageData.data[srcIndex + p];
+                for (let p = 0; p < 3; p++) {
+                    dest[destIndex + p] = imageData.data[srcIndex + p]
                 }
             }
         }
 
-        return new ImageData(dest, w, h);
+        return new ImageData(dest, w, h)
     }
 
     // noinspection JSUnusedGlobalSymbols
     return {
-        getImage: getImage,
-        makeCanvasSquare: makeCanvasSquare,
-        drawIntoCanvas: drawIntoCanvas,
-        calcScale: calcScale,
-        scaleImageData: scaleImageData,
-        getImageData: getImageData,
-        getPrescaledImageData: getPrescaledImageData,
+        getImage,
+        makeCanvasSquare,
+        drawIntoCanvas,
+        calcScale,
+        scaleImageData,
+        getImageData,
+        getPrescaledImageData,
         onMouseDown: onMouseDownDelta,
         onMouseDownAbsolute: onMouseDownAbsolute,
-        onScroll: onScroll,
-        getSquareAtCoords: getSquareAtCoords,
-        applyBorder: applyBorder
-    };
-}();
+        onScroll,
+        getSquareAtCoords,
+        applyBorder
+    }
+})();
